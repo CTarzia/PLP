2c4022e7522021a12fbf25a2a6b2905b1f675345
Tobias Agustin Carreira Munich <tcarreira@dc.uba.ar>
2020-05-19 19:26:15 -0300

    avanzamo

--- Inferencia/TypeInference.hs
+++ Inferencia/TypeInference.hs
@@ -29,8 +29,41 @@ infer' :: PlainExp -> Int -> Result (Int, TypingJudgment)
 
 -- COMPLETAR DESDE AQUI
 
-infer' (VarExp x)     n = undefined
-
+infer' (VarExp x)     n = OK (n + 1,(
+                                  extendC emptyContext x (TVar n),
+                                  (VarExp x),
+                                  (TVar n)
+                               )
+                          )
+infer' ZeroExp        n = OK (n, (
+                                  emptyContext,
+                                  ZeroExp,
+                                  TNat
+                               )
+                          )
+infer' (AppExp u v)   n = 
+    case infer' u n of
+        OK (n', (c', e', t')) ->
+            case infer' v n' of
+                OK (n'', (c'', e'', t'')) -> 
+                    case mgu (
+                        (t', (TFun t'' (TVar n''))) 
+                        : [(evalC var1 c', evalC var2 c'') |
+                            var1 <- domainC c',
+                            var2 <- domainC c'',
+                            var1 == var2]) of
+                            UOK subst -> OK (
+                                n'' + 1,
+                                (
+                                    joinC [subst <.> c', subst <.> c''],
+                                    subst <.> (AppExp e' e''),
+                                    subst <.> (TVar n'')
+                                )
+                            )
+                            UError u1 u2 -> uError u1 u2
+                res@(Error _) -> res
+        res@(Error _) -> res
+-- infer' LamExp
 
 
 
