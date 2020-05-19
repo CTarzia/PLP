7de31b93272e9e20c89a4571c70c9c72a4005ef4
Tobias Agustin Carreira Munich <tcarreira@dc.uba.ar>
2020-05-19 20:05:06 -0300

    Terminamos Lambdas, Aplicacion y Zero

--- Inferencia/TypeInference.hs
+++ Inferencia/TypeInference.hs
@@ -43,28 +43,53 @@ infer' ZeroExp        n = OK (n, (
                           )
 infer' (AppExp u v)   n = 
     case infer' u n of
-        OK (n', (c', e', t')) ->
-            case infer' v n' of
-                OK (n'', (c'', e'', t'')) -> 
+        OK (nu, (cu, u', tu)) ->
+            case infer' v nu of
+                OK (nv, (cv, v', tv)) -> 
                     case mgu (
-                        (t', (TFun t'' (TVar n''))) 
-                        : [(evalC var1 c', evalC var2 c'') |
-                            var1 <- domainC c',
-                            var2 <- domainC c'',
-                            var1 == var2]) of
-                            UOK subst -> OK (
-                                n'' + 1,
+                        (tu, (TFun tv (TVar nv))) 
+                        : [(evalC cu var1, evalC cv var2) |
+                            var1 <- domainC cu,
+                            var2 <- domainC cv,
+                            var1 == var2]
+                    ) of
+                            UOK subst ->
+                                OK
                                 (
-                                    joinC [subst <.> c', subst <.> c''],
-                                    subst <.> (AppExp e' e''),
-                                    subst <.> (TVar n'')
+                                    nv + 1,
+                                    (
+                                        joinC [subst <.> cu, subst <.> cv],
+                                        subst <.> (AppExp u' v'),
+                                        subst <.> (TVar nv)
+                                    )
                                 )
-                            )
                             UError u1 u2 -> uError u1 u2
                 res@(Error _) -> res
         res@(Error _) -> res
--- infer' LamExp
 
+infer' (LamExp x () e)     n =
+    case infer' e n of
+        OK ( n', (c', e', t') ) -> 
+            OK (
+                nRes, (
+                    removeC c' x,
+                    (LamExp x xType e'),
+                    (TFun xType t')
+                )
+            )
+            where nRes = n' + if x `elem` domainC c' then 1 else 0
+                  xType = if x `elem` domainC c' then evalC c' x else (TVar n')
+        res@(Error _) -> res
+
+
+infer' (SuccExp e) n =
+    case infer' e n of
+        OK ( n', (c', e', t') ) ->
+            case mgu [ (t', TNat) ] of
+                UOK subst ->
+                    OK ( n', (subst <.> c',subst <.> SuccExp e',TNat) )
+                UError u1 u2 ->uError u1 u2
+        res@(Error _) -> res
 
 
 --------------------------------
